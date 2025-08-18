3baa82f4bcdfba28ef373fcdd184cace91f343c5
BruceWayne-coder <vadimputin001@gmail.com>
2025-08-18 11:50:15 +0530

    Add files via upload

---
+++ LAB-1/1024160054_Shivansh/4b.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int r1, c1, r2, c2;
+    int A[10][10], B[10][10], C[10][10] = {0};
+
+    cout << "Enter rows and columns for matrix A: ";
+    cin >> r1 >> c1;
+    cout << "Enter elements of matrix A:\n";
+    for (int i = 0; i < r1; i++)
+        for (int j = 0; j < c1; j++)
+            cin >> A[i][j];
+
+    cout << "Enter rows and columns for matrix B: ";
+    cin >> r2 >> c2;
+
+    if (c1 != r2) {
+        cout << "Matrix multiplication not possible.\n";
+        return 0;
+    }
+
+    cout << "Enter elements of matrix B:\n";
+    for (int i = 0; i < r2; i++)
+        for (int j = 0; j < c2; j++)
+            cin >> B[i][j];
+
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c2; j++) {
+            for (int k = 0; k < c1; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+
+    cout << "Result of matrix multiplication:\n";
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c2; j++) cout << C[i][j] << " ";
+        cout << "\n";
+    }
+    return 0;
+}
